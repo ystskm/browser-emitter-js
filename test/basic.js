cb2baba303e0ed87178ee4da0e100c104b7a998c
ystskm <brilliantpenguin@gmail.com>
2014-01-08 11:58:42 +0900

    fix test

--- test/basic.js
+++ test/basic.js
@@ -17,9 +17,9 @@ module.exports = nodeunit.testCase({
       t.equal(typeof Emitter, 'function');
       var emitter = new Emitter();
       emitter.on('readme-test', function() {
-        t.equal(arguments[0], 1);
+        t.equal(arguments[0], 0);
         t.equal(arguments[1], 1);
-        t.equal(arguments[2], 1);
+        t.equal(arguments[2], 2);
         t.done();
       });
       emitter.emit('readme-test', 0, 1, 2);
