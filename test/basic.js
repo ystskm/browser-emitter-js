58124f2d86febe55c0cdd9c7fc8daaea38e03e34
ystskm <brilliantpenguin@gmail.com>
2015-05-23 19:15:55 +0900

    add test

--- test/basic.js
+++ test/basic.js
@@ -11,11 +11,29 @@ function setup(callback) {
 }
 
 module.exports = nodeunit.testCase({
-  'readme': function(t) {
+  'readme0': function(t) {
     setup(function(window) {
       var Emitter = window.Emitter;
       t.equal(typeof Emitter, 'function');
       var emitter = new Emitter();
+      emitter.on('readme-test', function() {
+        t.equal(arguments[0], 0);
+        t.equal(arguments[1], 1);
+        t.equal(arguments[2], 2);
+        t.done();
+      });
+      emitter.emit('readme-test', 0, 1, 2);
+    });
+  },
+  'readme_inherits': function(t) {
+    setup(function(window) {
+      var Emitter = window.Emitter;
+      t.equal(typeof Emitter, 'function');
+      var Super = function() {
+        Emitter.call(this);
+      };
+      Emitter.inherits(Super);
+      var emitter = new Super();
       emitter.on('readme-test', function() {
         t.equal(arguments[0], 0);
         t.equal(arguments[1], 1);
